b244a96079a95646bbfa90d931700b133aa3384c
LuizMaestri <luizricardomaestri@gmail.com>
2016-05-31 02:45:35 -0300

    add reminder model

---
+++ src/Events/model/Reminder.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: luiz
+ * Date: 30/05/16
+ * Time: 23:41
+ */
+
+namespace Events\model;
+
+
+class Reminder{
+    private $id;
+    private $description;
+    private $author;
+    private $period;
+
+    /**
+     * Reminder constructor.
+     */
+    public function __construct(){
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * @param mixed $author
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPeriod()
+    {
+        return $this->period;
+    }
+
+    /**
+     * @param mixed $period
+     */
+    public function setPeriod($period)
+    {
+        $this->period = $period;
+    }
+
+}
