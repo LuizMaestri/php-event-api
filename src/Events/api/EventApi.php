f815765cc59a22171216b7ee03210cccf7dce69e
LuizMaestri <luizricardomaestri@gmail.com>
2016-06-01 00:31:38 -0300

    create reminder api and fix event api urls

--- src/Events/api/EventApi.php
+++ src/Events/api/EventApi.php
@@ -16,33 +16,33 @@ class EventApi{
 
     public static function setRoute($app){
         /** @var App $app */
-        $app->get('/event/get/:id', function ($request, $response, $id){
+        $app->get('/event(/(:id))', function ($id){
             self::get($id);
         });
-        $app->get('/event/list/', function ($request, $response, $args){
+        $app->get('/events(/)', function (){
             self::listAll();
         });
-        $app->post('/event/create/', function ($request, $response, $args){
+        $app->post('/event(/)', function ($args){
             self::create($args);
         });
-        $app->put('/event/edit/', function ($request, $response, $args){
+        $app->put('/event(/)', function ($args){
             self::update($args);
         });
-        $app->delete('/event/delete/:id', function ($request, $response, $id){
+        $app->delete('/event(/(:id))', function ($id){
             self::delete($id);
         });
     }
 
     public static function get($id){
-
+        
     }
 
     public static function listAll(){
-
+        
     }
 
     public static function create($event){
-
+        
     }
 
     public static function update($event){
--- src/Events/api/ReminderApi.php
+++ src/Events/api/ReminderApi.php
@@ -8,8 +8,46 @@
 
 namespace Events\api;
 
+use Slim\App;
 
-class ReminderApi
-{
+class ReminderApi {
 
+    public static function setRoute($app){
+        /** @var App $app */
+        $app->get('/reminder(/(:id))', function ($id){
+            self::get($id);
+        });
+        $app->get('/reminders(/)', function (){
+            self::listAll();
+        });
+        $app->post('/reminder(/)', function ($args){
+            self::create($args);
+        });
+        $app->put('/reminder(/)', function ($args){
+            self::update($args);
+        });
+        $app->delete('/reminder(/(:id))', function ($id = null){
+            self::delete($id);
+        });
+    }
+
+    public static function get($id){
+        
+    }
+
+    public static function listAll(){
+        
+    }
+
+    public static function create($event){
+        
+    }
+
+    public static function update($event){
+        
+    }
+
+    public static function delete($id){
+        
+    }
 }
