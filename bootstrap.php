9e0bdc7b0d9c487edce86ccd25c82f833814bee4
LuizMaestri <luizricardomaestri@gmail.com>
2016-06-01 00:33:47 -0300

    doctrine config files

--- bootstrap.php
+++ bootstrap.php
@@ -5,3 +5,23 @@
  * Date: 31/05/16
  * Time: 22:42
  */
+
+// bootstrap.php
+require_once "vendor/autoload.php";
+
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+$paths = array(__DIR__."/src/Events/model");
+$isDevMode = false;
+
+// the connection configuration
+$dbParams = array(
+    'driver'   => 'mysqli',
+    'user'     => 'root',
+    'password' => '',
+    'dbname'   => 'testevent',
+);
+
+$config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
+$entityManager = EntityManager::create($dbParams, $config);
--- cli-config.php
+++ cli-config.php
@@ -5,3 +5,5 @@
  * Date: 31/05/16
  * Time: 22:41
  */
+require_once "bootstrap.php";
+return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet($entityManager);
